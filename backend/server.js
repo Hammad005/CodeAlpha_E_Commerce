057a20c8f83f0883da743663939fcb4c3079e1f7
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-02-17 20:03:54 +0500

    changes in cors policy

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.json({limit: "50mb"}));
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }));
 
 
 app.use("/api/auth", authRoutes);
