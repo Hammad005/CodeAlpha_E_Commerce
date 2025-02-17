cc01aa33ba9364e53cb56bd447f2cb66d4c91932
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-02-17 19:58:30 +0500

    cors policy updates

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json({limit: "50mb"}));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 
 app.use("/api/auth", authRoutes);
