01f0242780ba1692c4923811a85e80240fd19a62
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-02-18 14:52:15 +0500

    changes in db

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log("Error Connecting to MongoDB", error.message);
-        process.exit(1);
-           
+  try {
+    if (mongoose.connection.readyState === 1) {
+      console.log("Already connected to MongoDB");
+      return;
     }
-}
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
